bd084304c317e37ff48cc07b485e839622e6d0d9
Mehmed Cherkez <m3dkata_tr@abv.bg>
2024-12-12 21:10:15 +0200

    Update context_processors.py

--- config/context_processors.py
+++ config/context_processors.py
@@ -5,7 +5,7 @@ from apps.reports.models import Report
 def report_context(request):
     context = {'has_today_report': True}
     
-    if request.user.is_authenticated and request.user.is_employee:
+    if hasattr(request, 'user') and request.user.is_authenticated and request.user.is_employee:
         try:
             employee = Employee.objects.select_related('user').get(user=request.user)
             context['has_today_report'] = Report.objects.select_related('employee').filter(
@@ -18,7 +18,7 @@ def report_context(request):
     return context
 
 def employee_status_context(request):
-    if request.user.is_authenticated and request.user.is_staff:
+    if hasattr(request, 'user') and request.user.is_authenticated and request.user.is_staff:
         total_active = Employee.objects.filter(user__is_active=True).count()
         working_now = Employee.objects.filter(user__is_active=True, is_online=True).count()
         return {
